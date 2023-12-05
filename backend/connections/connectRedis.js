2bc98b57e1531fa4d20526644bd173dd66d83149
Madan <madangopal16072000@gmail.com>
2023-12-05 11:32:49 +0530

    added remote redis

--- backend/connections/connectRedis.js
+++ backend/connections/connectRedis.js
@@ -3,7 +3,7 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const connect = async () => {
   const client = await redis
-    .createClient(REDIS_PORT)
+    .createClient(process.env.REDIS_URL)
     .on("error", (err) => console.log("Redis connection Error", err))
     .connect();
 
